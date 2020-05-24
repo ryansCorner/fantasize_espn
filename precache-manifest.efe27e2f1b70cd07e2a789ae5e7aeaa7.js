4e0abc56de11ae77cc7fd47ddf3a12ceb3fb6f09
Ryan Felter <42056419+ryansCorner@users.noreply.github.com>
2020-05-23 23:05:51 -0700

    Updates

---
+++ precache-manifest.efe27e2f1b70cd07e2a789ae5e7aeaa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba14c809b5e8b8aef899a5a0de7d7d6",
+    "url": "/fantasize_espn/index.html"
+  },
+  {
+    "revision": "74ba2b6f6f3f5d72e041",
+    "url": "/fantasize_espn/static/css/main.fa95fa3c.chunk.css"
+  },
+  {
+    "revision": "e693b8a27628e2eec55e",
+    "url": "/fantasize_espn/static/js/2.626e8d8f.chunk.js"
+  },
+  {
+    "revision": "74ba2b6f6f3f5d72e041",
+    "url": "/fantasize_espn/static/js/main.e668d49b.chunk.js"
+  },
+  {
+    "revision": "298b454503eb1b06c69d",
+    "url": "/fantasize_espn/static/js/runtime~main.a37078da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fantasize_espn/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ryansCorner/fantasize_espn/precache-manifest.5badcbfab891a9158d3bc78c7217ff51.js"
+  "/fantasize_espn/precache-manifest.efe27e2f1b70cd07e2a789ae5e7aeaa7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ryansCorner/fantasize_espn/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fantasize_espn/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
