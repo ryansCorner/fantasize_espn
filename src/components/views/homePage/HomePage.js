9a7fd2371fca730b633e1f763d1232e7b5d0fd98
Ryan Felter <42056419+ryansCorner@users.noreply.github.com>
2020-05-25 12:16:14 -0700

    homepage magin adjusted

--- src/components/views/homePage/HomePage.js
+++ src/components/views/homePage/HomePage.js
@@ -6,10 +6,13 @@ import StandingsTable from '../../tables/StandingsTable'
 const HomePage = props => {
     return (
         <React.Fragment>
-            <div>
-                <h1>Welcome to {props.leagueName}</h1>
+            <div className='homepage-container'>
+                <div className='homepage-header'>
 
-                <h2>Final Season Standings</h2>
+                    <h1>Welcome to {props.leagueName}</h1>
+
+                    <h2>Final Season Standings</h2>
+                </div>
                 <StandingsTable
                     teams={props.teams}
                     onTeamClick={props.onTeamClick}
